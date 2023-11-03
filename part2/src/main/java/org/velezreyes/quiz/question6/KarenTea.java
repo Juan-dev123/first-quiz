656e49caaa948b83be55d900771e87a622273960
Juan Pablo Ramos Torres <378tech@gmail.com>
2023-11-02 21:47:53 -0500

    The part 2 was resolved

---
+++ part2/src/main/java/org/velezreyes/quiz/question6/KarenTea.java
@@ -0,0 +1,19 @@
+package org.velezreyes.quiz.question6;
+
+public class KarenTea implements Drink {
+
+    @Override
+    public String getName() {
+        return "KarenTea";
+    }
+
+    @Override
+    public boolean isFizzy() {
+        return false;
+    }
+
+    @Override
+    public double getPrice() {
+        return 1;
+    }
+}
--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,51 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+public class VendingMachineImpl implements VendingMachine {
+
+  private static volatile VendingMachineImpl instance;
+  private double insertedMoney;
+
+  private VendingMachineImpl() {
+    insertedMoney = 0;
+  }
 
   public static VendingMachine getInstance() {
-    // Fix me!
-    return null;
+    if (instance != null) {
+      return instance;
+    }
+    synchronized (VendingMachineImpl.class) {
+      if (instance == null) {
+        instance = new VendingMachineImpl();
+      }
+      return instance;
+    }
+  }
+
+  @Override
+  public void insertQuarter() {
+    insertedMoney += 0.25;
+  }
+
+  @Override
+  public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+    switch (name) {
+      case "ScottCola":
+        Drink scottCola = new ScottCola();
+        buyDrink(scottCola);
+        return scottCola;
+      case "KarenTea":
+        Drink karenTea = new KarenTea();
+        buyDrink(karenTea);
+        return karenTea;
+      default:
+        throw new UnknownDrinkException();
+    }
+  }
+
+  private void buyDrink(Drink drink) throws NotEnoughMoneyException {
+    if (insertedMoney < drink.getPrice()) {
+      throw new NotEnoughMoneyException();
+    }
+    insertedMoney -= drink.getPrice();
   }
 }
